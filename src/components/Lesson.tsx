18cdd6db2e91afb0ff76ec73c049c2c34730e79a
vandodev <vandoaparecido@hotmail.com>
2023-08-02 08:23:41 -0300

    :rocket: Selecionando aula atual (Segunda  forma)

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,21 +1,14 @@
 import { Video } from "lucide-react";
-import { useDispatch } from "react-redux";
-import { play } from "../store/slices/player";
 
 interface LessonProps {
   title: string;
   duration: string;
+  onPlay: () => void;
 }
 
-export function Lesson({ title, duration }: LessonProps) {
-  const dispatch = useDispatch();
-
-function handlePlayLesson(){
- dispatch(play("teste"));
-}
-
+export function Lesson({ title, duration, onPlay }: LessonProps) {
   return (
-    <button onClick={handlePlayLesson} className="flex items-center gap-3 text-sm text-zinc-400">
+    <button onClick={onPlay}  className="flex items-center gap-3 text-sm text-zinc-400">
       <Video className="w-4 h-4 text-zinc-500" />
       <span>{title}</span>
       <span className="ml-auto font-mono text-xs text-zinc-500">{duration}</span>
--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -2,6 +2,8 @@ import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
 import * as Collapsible from '@radix-ui/react-collapsible';
 import { useAppSelector } from "../store";
+import { useDispatch } from "react-redux";
+import { play } from "../store/slices/player";
 
 interface ModuleProps {
   moduleIndex: number;
@@ -10,6 +12,7 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
+const dispatch = useDispatch()
 
   const lessons = useAppSelector(state => {
     return state.player.course.modules[moduleIndex].lessons
@@ -32,12 +35,13 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
 
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map((lesson) => {
+          {lessons.map((lesson, lessonIndex) => {
             return (
               <Lesson
                 key={lesson.id}
                 title={lesson.title}
                 duration={lesson.duration}
+                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
               />
             )
           })}
