208ce165fbc5cc04121f78ff57ab26aac9a49af1
vandodev <vandoaparecido@hotmail.com>
2023-08-02 08:11:02 -0300

    :rocket: Selecionando aula atual (primeira forma)

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,4 +1,6 @@
 import { Video } from "lucide-react";
+import { useDispatch } from "react-redux";
+import { play } from "../store/slices/player";
 
 interface LessonProps {
   title: string;
@@ -6,8 +8,14 @@ interface LessonProps {
 }
 
 export function Lesson({ title, duration }: LessonProps) {
+  const dispatch = useDispatch();
+
+function handlePlayLesson(){
+ dispatch(play("teste"));
+}
+
   return (
-    <button className="flex items-center gap-3 text-sm text-zinc-400">
+    <button onClick={handlePlayLesson} className="flex items-center gap-3 text-sm text-zinc-400">
       <Video className="w-4 h-4 text-zinc-500" />
       <span>{title}</span>
       <span className="ml-auto font-mono text-xs text-zinc-500">{duration}</span>
