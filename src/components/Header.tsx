40f7c11a6eede7a1a97b96363fdcc65de23620c6
vandodev <vandoaparecido@hotmail.com>
2023-08-04 08:47:35 -0300

    :rocket: Criando interface de loading

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,18 @@
 import { useCurrentLesson } from "../store/slices/player";
-
+import { useAppSelector } from "../store";
 
 export function Header() {
- 
+  const isCourseLoading = useAppSelector(state => state.player.isLoading)
   const { currentModule, currentLesson } = useCurrentLesson();
 
-  if (!currentModule || !currentLesson) {
-    return null
+  if (isCourseLoading) {
+    return <h1 className="text-2xl font-bold">Carregando...</h1>
   }
 
     return (
       <div className="flex flex-col gap-1">
-       <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
-      <span className="text-sm text-zinc-400">Módulo "{currentModule.title}"</span>
+        <h1 className="text-2xl font-bold">{currentLesson?.title}</h1>
+        <span className="text-sm text-zinc-400">Módulo "{currentModule?.title}"</span>
       </div>
     );
   }
