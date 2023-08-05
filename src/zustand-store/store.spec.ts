82cc29a3a6fdfd6bfc88d2a0c623a25adb651f1b
vandodev <vandoaparecido@hotmail.com>
2023-08-05 12:17:59 -0300

    :rocket: Testando de vai para o próximo vídeo altomáticamente com zustand

--- src/zustand-store/store.spec.ts
+++ src/zustand-store/store.spec.ts
@@ -1,12 +1,55 @@
-import { describe, expect, it } from "vitest";
-import { useStore } from '.';
+import { beforeEach, describe, expect, it } from "vitest";
+import { useStore as store } from '.'
+
+const course = {
+    id: 1,
+    modules: [
+      {
+        id: 1,
+        title: 'Iniciando com React',
+        lessons: [
+          { id: 'Jai8w6K_GnY', title: 'CSS Modules', duration: '13:45' },
+          { id: 'w-DW4DhDfcw', title: 'Estilização do Post', duration: '10:05' },
+        ],
+      },
+      {
+        id: 2,
+        title: 'Estrutura da aplicação',
+        lessons: [
+          { id: 'gE48FQXRZ_o', title: 'Componente: Comment', duration: '13:45' },
+          { id: 'Ng_Vk4tBl0g', title: 'Responsividade', duration: '10:05' },
+        ],
+      },
+    ],
+  }
+  
+  const initialState = store.getState()
+
 
 describe('zustand store', () => {
+
+    beforeEach(() => {//Antes de testar zera as variaveis
+        store.setState(initialState)
+      })
+
     it('should be able to play', () => {
-        const {play} = useStore.getState();
+        const {play} = store.getState();
         play([1,2]);
-        const {currentModuleIndex, currentLessonIndex} = useStore.getState();
+        const {currentModuleIndex, currentLessonIndex} = store.getState();
         expect(currentModuleIndex).toEqual(1);
         expect(currentLessonIndex).toEqual(2)
     })
+
+    it('should be able to play next video automatically', () => {
+        store.setState({ course });
+
+        const { next } = store.getState();
+    
+        next();
+    
+        const { currentModuleIndex, currentLessonIndex } = store.getState();
+    
+        expect(currentModuleIndex).toEqual(0);
+        expect(currentLessonIndex).toEqual(1);
+      })
 })
